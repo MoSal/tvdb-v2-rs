ea40a3b25d7a6ca89d29da9c9a98feb29c0e614a
Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>
2020-01-25 02:16:54 +0200

    Use API v3
    Signed-off-by: Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ extern crate reqwest;
 
 const USER_AGENT: &str = "tvdb-v2-rs/0.1";
 const BASE_URL: &str = "https://api.thetvdb.com";
-const ACCEPT_API_VERSION: &str = "application/vnd.thetvdb.v2";
+const ACCEPT_API_VERSION: &str = "application/vnd.thetvdb.v3";
 
 mod tvdb_errors;
 mod tvdb_from;
